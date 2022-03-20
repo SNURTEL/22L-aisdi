8d9237981d5d2901fe8a1dda56006f4b1e9b7682
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-03-20 15:22:00 +0100

    Morse project structure

---
+++ 01-morse/tests/test_morse.py
@@ -0,0 +1,37 @@
+from src.morse import morse
+
+
+def test_one_letter():
+    pass
+
+
+def test_one_letter_lowercase():
+    pass
+
+
+def test_word():
+    pass
+
+
+def test_word_lowercase():
+    pass
+
+
+def test_sentence_1():
+    pass
+
+
+def test_sentence_lowercase():
+    pass
+
+
+def test_sentence_capitalize():
+    pass
+
+
+def test_multiline_1():
+    pass
+
+
+def test_multiline_2():
+    pass
