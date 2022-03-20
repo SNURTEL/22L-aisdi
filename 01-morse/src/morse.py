ca0488e360c3876a6271a804985a72159341d52b
Tomasz Owienko <tomek.owienko@gmail.com>
2022-03-20 18:05:13 +0100

    Implemented the Morse encoder

--- 01-morse/src/morse.py
+++ 01-morse/src/morse.py
@@ -1,10 +1,30 @@
+import sys
+import argparse
+
+from morse_encoder import DictMorseEncoder
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+    args = parser.parse_args()
+
+    path = args.path
+
+    with open(path, mode='r', encoding='utf-8') as fp:
+        encoded = morse(fp)
+
+    print(encoded)
+
+
 def morse(fp) -> str:
-    pass
+    plaintext = '\n'.join(fp.readlines())
+
+    dme = DictMorseEncoder(' ', '/')
+    encoded = dme.encode_string(plaintext)
+
+    return encoded
 
 
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    main(sys.argv)
