3644a07552b3a782b3c2aa1e18f9d2351dd8ae2f
jkowalc4 <01169189@pw.edu.pl>
2022-03-26 23:44:18 +0100

    Implement bubble and merge sort

--- lab02_sort/src/merge_sort.py
+++ lab02_sort/src/merge_sort.py
@@ -2,4 +2,24 @@ from typing import Any, List
 
 
 def merge_sort(array: List[Any]) -> List[Any]:
-    pass  # TODO implement
+    if len(array) == 1:
+        return array
+
+    div_point = len(array)//2
+
+    first_part_sorted = merge_sort(array[:div_point])
+    second_part_sorted = merge_sort(array[div_point:])
+
+    return merge(first_part_sorted, second_part_sorted)
+
+
+def merge(first_part: List[Any], second_part: List[Any]):
+    result = []
+    while len(first_part) > 0 and len(second_part) > 0:
+        if(first_part[0] > second_part[0]):
+            result.append(second_part.pop(0))
+        else:
+            result.append(first_part.pop(0))
+    result.extend(first_part)
+    result.extend(second_part)
+    return result
