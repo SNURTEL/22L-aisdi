148e2db727736725846b3b195a883f4449847c65
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-03-26 23:36:29 +0100

    Rewrite merge sort

--- lab02_sort/src/merge_sort.py
+++ lab02_sort/src/merge_sort.py
@@ -4,12 +4,22 @@ from typing import Any, List
 def merge_sort(array: List[Any]) -> List[Any]:
     if len(array) == 1:
         return array
-    div_point = floor(len(array)/2)
-    first_partition = array[:div_point]
-    second_partition = array[div_point:]
-    sorted_first_partition = merge_sort(first_partition)
-    sorted_second_partition = merge_sort(second_partition)
-    if first_partition[0] > second_partition[0]:
-        return sorted_second_partition.extend(sorted_first_partition)
-    else:
-        return sorted_first_partition.extend(sorted_second_partition)
+
+    div_point = len(array)//2
+
+    first_part_sorted = merge_sort(array[:div_point])
+    second_part_sorted = merge_sort(array[div_point:])
+
+    return merge(first_part_sorted, second_part_sorted)
+
+
+def merge(first_part: List[Any], second_part: List[Any]):
+    result = []
+    while len(first_part) > 0 and len(second_part) > 0:
+        if(first_part[0] > second_part[0]):
+            result.append(second_part.pop(0))
+        else:
+            result.append(first_part.pop(0))
+    result.extend(first_part)
+    result.extend(second_part)
+    return result
