76db4e721a64e43f294fa7b0aa4b95742a4139e9
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-03-26 22:56:17 +0100

    Add bubble sort and merge sort prototypes

--- lab02_sort/src/bubble_sort.py
+++ lab02_sort/src/bubble_sort.py
@@ -2,4 +2,11 @@ from typing import Any, List
 
 
 def bubble_sort(array: List[Any]) -> List[Any]:
-    pass  # TODO Implement
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(array) - 1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                swapped = True
+    return array
--- lab02_sort/src/merge_sort.py
+++ lab02_sort/src/merge_sort.py
@@ -2,4 +2,14 @@ from typing import Any, List
 
 
 def merge_sort(array: List[Any]) -> List[Any]:
-    pass  # TODO implement
+    if len(array) == 1:
+        return array
+    div_point = floor(len(array)/2)
+    first_partition = array[:div_point]
+    second_partition = array[div_point:]
+    sorted_first_partition = merge_sort(first_partition)
+    sorted_second_partition = merge_sort(second_partition)
+    if first_partition[0] > second_partition[0]:
+        return sorted_second_partition.extend(sorted_first_partition)
+    else:
+        return sorted_first_partition.extend(sorted_second_partition)
