d6e9df58fb72397e5613ff60a17de9391fb16f0d
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-03-30 11:52:50 +0200

    Recover morse

---
+++ lab01_morse/src/morse.py
@@ -0,0 +1,30 @@
+import sys
+import argparse
+
+from .morse_encoder import DictMorseEncoder
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+    args = parser.parse_args()
+
+    path = args.path
+
+    with open(path, mode='r', encoding='utf-8') as fp:
+        encoded = morse(fp)
+
+    print(encoded)
+
+
+def morse(fp) -> str:
+    plaintext = ''.join(fp.readlines())
+
+    dme = DictMorseEncoder(' ', '/')
+    encoded = dme.encode_string(plaintext)
+
+    return encoded
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ lab01_morse/src/morse_encoder.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+import re
+
+
+class MorseEncoder(ABC):
+    def __init__(self, letter_separator: str, word_separator: str):
+        self._letter_separator = letter_separator
+        self._word_separator = word_separator
+
+    @staticmethod
+    def filter_invalid_characters(plaintext: str):
+        return re.sub('( ){2,}', ' ', re.sub('[^a-zA-z\n ]|\\\\', '', plaintext))
+
+    @abstractmethod
+    def encode_string(self, plaintext: str) -> str:
+        pass
+
+
+class DictMorseEncoder(MorseEncoder):
+    def __init__(self, letter_separator: str, word_separator: str):
+        super(DictMorseEncoder, self).__init__(letter_separator, word_separator)
+
+        self._ascii_to_morse_character_table = {'A': '.-',
+                                                'B': '-...',
+                                                'C': '-.-.',
+                                                'D': '-..',
+                                                'E': '.',
+                                                'F': '..-.',
+                                                'G': '--.',
+                                                'H': '....',
+                                                'I': '..',
+                                                'J': '.---',
+                                                'K': '-.-',
+                                                'L': '.-..',
+                                                'M': '--',
+                                                'N': '-.',
+                                                'O': '---',
+                                                'P': '.--.',
+                                                'Q': '--.-',
+                                                'R': '.-.',
+                                                'S': '...',
+                                                'T': '-',
+                                                'U': '..-',
+                                                'V': '...-',
+                                                'X': '-..-',
+                                                'Y': '-.--',
+                                                'Z': '--..',
+                                                ' ': self._word_separator,
+                                                '\n': '\n'}
+
+    def encode_string(self, plaintext: str) -> str:
+        return '\n'.join(
+            [self._letter_separator.join([self._ascii_to_morse_character_table[char]
+                                          for char in line])
+             for line in
+             MorseEncoder.filter_invalid_characters(plaintext.upper()).split('\n')])
