082329813a91f769b8a2e7d633a997eafe161bcb
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-05-23 16:11:12 +0200

    Add better variable names in KMP

--- lab05_pattern_recognition/src/findKMP.py
+++ lab05_pattern_recognition/src/findKMP.py
@@ -3,32 +3,32 @@ from typing import List
 
 def generate_pi(pattern: str) -> List[int]:
     pi = [0]
-    k = 0
-    for q in range(1, len(pattern)):
-        while k > 0 and pattern[k] != pattern[q]:
-            k = pi[k-1]
-        if pattern[k] == pattern[q]:
-            k += 1
-        pi.append(k)
+    prefix_len = 0
+    for substring_len in range(1, len(pattern)):
+        while prefix_len > 0 and pattern[prefix_len] != pattern[substring_len]:
+            prefix_len = pi[prefix_len-1]
+        if pattern[prefix_len] == pattern[substring_len]:
+            prefix_len += 1
+        pi.append(prefix_len)
     return pi
 
 
 def find_kmp(pattern: str, text: str) -> List[int]:
     if not len(pattern) or not len(text):
         return []
-    found = []
+    found_list = []
     pattern_len = len(pattern)
     pi = generate_pi(pattern)
-    q = 0
-    for i, char in enumerate(text):
-        while q > 0 and pattern[q] != char:  # Next char is wrong
-            q = pi[q-1]  # Decrement q to the right value
-        if pattern[q] == char:  # Next char is right
-            q += 1
-        if q == pattern_len:  # Found pattern
-            found.append(i - pattern_len + 1)
-            q = pi[q-1]  # Start next search
-    return found
+    matched_char_count = 0
+    for pos, char in enumerate(text):
+        while matched_char_count > 0 and pattern[matched_char_count] != char:  # Next char is wrong
+            matched_char_count = pi[matched_char_count-1]  # Decrement q to the right value
+        if pattern[matched_char_count] == char:  # Next char is right
+            matched_char_count += 1
+        if matched_char_count == pattern_len:  # Found pattern
+            found_list.append(pos - pattern_len + 1)
+            matched_char_count = pi[matched_char_count-1]  # Start next search
+    return found_list
 
 
 if __name__ == "__main__":
